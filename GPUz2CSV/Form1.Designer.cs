432d311ab4266abdbe28b126513e9c322d87abf4
tona_kriz <tonakriz@gmail.com>
2017-03-03 17:18:10 +0100

    First commit

---
+++ GPUz2CSV/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace GPUz2CSV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(14, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(315, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Convert GPUz log file to CSV";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.checkBox1.Location = new System.Drawing.Point(14, 77);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(320, 19);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Use comma \",\" instead of decimal point \".\"";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.checkBox2.Location = new System.Drawing.Point(14, 104);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(292, 19);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "Use semicolon \";\" instead of comma \",\"";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 137);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Name = "Form1";
+            this.Text = "GPUz2CSV by Tona_Kriz";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ GPUz2CSV/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Security;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GPUz2CSV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeOpenFileDialog();
+        }
+
+        private void InitializeOpenFileDialog()
+        {
+            openFileDialog1.Filter = "Text documents (*.txt)|*.txt|" + "All files (*.*)|*.*";
+            openFileDialog1.Title = "GPUz2CSV";
+            openFileDialog1.Multiselect = true;
+            openFileDialog1.FileName = "GPU-Z Sensor Log.txt";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            checkBox1.Enabled = false;
+            DialogResult dr = openFileDialog1.ShowDialog();
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                foreach (String file in openFileDialog1.FileNames)
+                {
+                    try
+                    {
+                        string content = File.ReadAllText(file, Encoding.UTF8);
+                        if (checkBox2.Checked)
+                        {
+                            if (checkBox1.Checked)
+                            {
+                                content = File.ReadAllText(file, Encoding.UTF8).Replace(",", ";").Replace(".", ",");
+                            } else
+                            {
+                                content = File.ReadAllText(file, Encoding.UTF8).Replace(",", ";");
+                            }
+                        } else if (checkBox1.Checked)
+                        {
+                            content = File.ReadAllText(file, Encoding.UTF8).Replace(".", ",");
+                        }
+                        string newfilepath = Path.GetDirectoryName(file) + @"\" + Path.GetFileNameWithoutExtension(file) + ".csv";
+
+                        File.CreateText(newfilepath).Close();
+                        File.WriteAllText(newfilepath, content, Encoding.UTF8);
+                    }
+                    catch (SecurityException ex)
+                    {
+                        MessageBox.Show("Security error. Please contact your administrator for details.\n\n" +
+                            "Error message: " + ex.Message + "\n\n" +
+                            "Details (send to Support):\n\n" + ex.StackTrace
+                        );
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Cannot edit the files: " + file.Substring(file.LastIndexOf('\\'))
+                            + ". You may not have permission to read the file, or " +
+                            "it may be corrupt.\n\nReported error: " + ex.Message);
+                    }
+                }
+            }
+            checkBox1.Enabled = true;
+            button1.Enabled = true;
+        }
+    }
+}
