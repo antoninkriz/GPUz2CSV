19c43ab7074cc668ffe6ab6dced37efc5281924b
tona_kriz <tonakriz@gmail.com>
2017-03-15 14:50:54 +0100

    Now with ability to edit all files in a folder

--- GPUz2CSV/Form1.Designer.cs
+++ GPUz2CSV/Form1.Designer.cs
@@ -32,16 +32,18 @@
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button1.Location = new System.Drawing.Point(14, 14);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(315, 57);
+            this.button1.Size = new System.Drawing.Size(161, 59);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Convert GPUz log file to CSV";
+            this.button1.Text = "Convert GPUz log files to CSV";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -53,7 +55,7 @@
             // 
             this.checkBox1.AutoSize = true;
             this.checkBox1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.checkBox1.Location = new System.Drawing.Point(14, 77);
+            this.checkBox1.Location = new System.Drawing.Point(12, 77);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(320, 19);
             this.checkBox1.TabIndex = 1;
@@ -64,24 +66,46 @@
             // 
             this.checkBox2.AutoSize = true;
             this.checkBox2.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.checkBox2.Location = new System.Drawing.Point(14, 104);
+            this.checkBox2.Location = new System.Drawing.Point(12, 102);
             this.checkBox2.Name = "checkBox2";
             this.checkBox2.Size = new System.Drawing.Size(292, 19);
             this.checkBox2.TabIndex = 2;
             this.checkBox2.Text = "Use semicolon \";\" instead of comma \",\"";
             this.checkBox2.UseVisualStyleBackColor = true;
             // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(12, 127);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(229, 19);
+            this.checkBox3.TabIndex = 3;
+            this.checkBox3.Text = "Output inside original folder";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(180, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 58);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Convert folder full of .txts logs to CSV";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(343, 137);
+            this.ClientSize = new System.Drawing.Size(343, 154);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox3);
             this.Controls.Add(this.checkBox2);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button1);
             this.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.Name = "Form1";
-            this.Text = "GPUz2CSV by Tona_Kriz";
+            this.Text = " GPUz2CSV";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -93,6 +117,8 @@
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- GPUz2CSV/Form1.cs
+++ GPUz2CSV/Form1.cs
@@ -26,52 +26,111 @@ namespace GPUz2CSV
         {
             button1.Enabled = false;
             checkBox1.Enabled = false;
+            checkBox2.Enabled = false;
+            checkBox3.Enabled = false;
+
             DialogResult dr = openFileDialog1.ShowDialog();
-            string savedir = dirdialog();
-            if (dr == System.Windows.Forms.DialogResult.OK)
+
+            string savedir = null;
+            if (!checkBox3.Checked)
             {
+                savedir = dirdialog();
+            }
+
+            if ((openFileDialog1.FileNames != null || (savedir != null && openFileDialog1.FileNames != null)) && dr == DialogResult.OK)
+            {
+                generateFiles(savedir);
+            }
+
+            button1.Enabled = true;
+            checkBox1.Enabled = true;
+            checkBox2.Enabled = true;
+            checkBox3.Enabled = true;
+
+        }
+
+        private void generateFiles(string savedir)
+        {
+            if (openFileDialog1.FileNames != null || savedir != null)
+            {
+                Console.WriteLine(openFileDialog1.FileNames);
+                Console.WriteLine(savedir);
                 foreach (String file in openFileDialog1.FileNames)
                 {
-                    try
-                    {
-                        string content = File.ReadAllText(file, Encoding.UTF8);
-                        if (checkBox2.Checked)
-                        {
-                            if (checkBox1.Checked)
-                            {
-                                content = File.ReadAllText(file, Encoding.UTF8).Replace(",", ";").Replace(".", ",");
-                            } else
-                            {
-                                content = File.ReadAllText(file, Encoding.UTF8).Replace(",", ";");
-                            }
-                        } else if (checkBox1.Checked)
-                        {
-                            content = File.ReadAllText(file, Encoding.UTF8).Replace(".", ",");
-                        }
-                        string newfilepath = savedir + @"\" + Path.GetFileNameWithoutExtension(file) + ".csv";
+                    genfiles(file, savedir);
+                }
+            }
+        }
 
-                        File.CreateText(newfilepath).Close();
+        private void genfiles(string file, string savedir)
+        {
+            try
+            {
+                string content = File.ReadAllText(file, Encoding.UTF8);
+                if (checkBox2.Checked)
+                {
+                    if (checkBox1.Checked)
+                    {
+                        content = File.ReadAllText(file, Encoding.UTF8).Replace(",", ";").Replace(".", ",");
+                    }
+                    else
+                    {
+                        content = File.ReadAllText(file, Encoding.UTF8).Replace(",", ";");
+                    }
+                }
+                else if (checkBox1.Checked)
+                {
+                    content = File.ReadAllText(file, Encoding.UTF8).Replace(".", ",");
+                }
 
-                        File.WriteAllText(newfilepath, content, Encoding.UTF8);
-                    }
-                    catch (SecurityException ex)
+                string newfilepath;
+                if (checkBox3.Checked)
+                {
+                    newfilepath = Path.GetDirectoryName(file) + @"\" + Path.GetFileNameWithoutExtension(file) + ".csv";
+                }
+                else
+                {
+                    newfilepath = savedir + @"\" + Path.GetFileNameWithoutExtension(file) + ".csv";
+                }
+
+
+                File.CreateText(newfilepath).Close();
+
+                File.WriteAllText(newfilepath, content, Encoding.UTF8);
+            }
+            catch (SecurityException ex)
+            {
+                MessageBox.Show("Security error. Please contact your administrator for details.\n\n" +
+                    "Error message: " + ex.Message + "\n\n" +
+                    "Details (send to Support):\n\n" + ex.StackTrace
+                );
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Cannot edit the files: " + file.Substring(file.LastIndexOf('\\'))
+                    + ". You may not have permission to read the file, or " +
+                    "it may be corrupt.\n\nReported error: " + ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string savedir = null;
+            savedir = dirdialog();
+            if (savedir != null)
+            {
+                string[] files = Directory.GetFiles(savedir, "*.txt");
+                if (files != null)
+                {
+                    foreach (String file in files)
                     {
-                        MessageBox.Show("Security error. Please contact your administrator for details.\n\n" +
-                            "Error message: " + ex.Message + "\n\n" +
-                            "Details (send to Support):\n\n" + ex.StackTrace
-                        );
-                    }
-                    catch (Exception ex)
-                    {
-                        MessageBox.Show("Cannot edit the files: " + file.Substring(file.LastIndexOf('\\'))
-                            + ". You may not have permission to read the file, or " +
-                            "it may be corrupt.\n\nReported error: " + ex.Message);
+                        genfiles(file, savedir);
                     }
                 }
             }
-            checkBox1.Enabled = true;
-            button1.Enabled = true;
+
         }
+
 
         private string dirdialog()
         {
@@ -83,11 +142,10 @@ namespace GPUz2CSV
                 {
                     return fbd.SelectedPath;
                 }
-                else {
-                    dirdialog();
-                }
-                return fbd.SelectedPath;
-            }   
+                return null;
+            }
         }
+
+
     }
 }
